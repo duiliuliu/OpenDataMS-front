fdc29559f411cf25da5f4def5f5556f7ae5cd510
pengrui <pengrui55555@163.com>
2019-03-05 09:48:41 +0800

    更新 配置eslint

--- opendata-ms/src/App.js
+++ opendata-ms/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,15 +7,17 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img alt="logo"
+              className="App-logo"
+              src={logo} />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+              className="App-link"
+              href="https://reactjs.org"
+              rel="noopener noreferrer"
+              target="_blank"
           >
             Learn React
           </a>
