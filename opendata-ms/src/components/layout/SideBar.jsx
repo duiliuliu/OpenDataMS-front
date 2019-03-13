ade002ad88e01f43567aad2b17699e82d249542d
pengrui <pengrui55555@163.com>
2019-03-13 09:58:51 +0800

    Update SideBar.jsx

--- opendata-ms/src/components/layout/SideBar.jsx
+++ opendata-ms/src/components/layout/SideBar.jsx
@@ -3,16 +3,13 @@ import { Link, withRouter } from 'react-router-dom';
 import { Layout, Menu, Icon } from 'antd';
 const { Sider } = Layout;
 const { SubMenu, Item } = Menu;
-import { routerData, groupKey } from '../../router/data';
+import { routerData } from '../../router/data';
 
 class Sidebar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      openKeys: [''],
-      selectedKeys: [''],
-      rootSubmenuKeys: groupKey,
-      itemName: ''
+      selectedKeys: ['']
     };
   }
 
@@ -44,21 +41,8 @@ class Sidebar extends Component {
     });
   };
 
-  OpenChange = openKeys => {
-    const latestOpenKey = openKeys.find(
-      key => this.state.openKeys.indexOf(key) === -1
-    );
-    if (this.state.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-      this.setState({ openKeys });
-    } else {
-      this.setState({
-        openKeys: latestOpenKey ? [latestOpenKey] : [...openKeys]
-      });
-    }
-  };
-
   render() {
-    const { openKeys, selectedKeys } = this.state;
+    const { selectedKeys } = this.state;
     const { collapsed } = this.props;
     const SideTree = routerData.map(item => (
       <SubMenu
@@ -94,8 +78,6 @@ class Sidebar extends Component {
         <Menu
             className="menu"
             mode="inline"
-            onOpenChange={this.OpenChange}
-            openKeys={openKeys}
             selectedKeys={selectedKeys}
             style={{ height: '100%', borderRight: 0 }}
             subMenuOpenDelay={0.3}
